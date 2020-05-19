2e9d4697c5fa60eedb339086bd87f15886826441
Tiina <malitsu@gmail.com>
2020-05-20 01:26:57 +0300

    Prepare the app for Heroku.

--- server.js
+++ server.js
@@ -1,22 +1,14 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
 
-const api = require('./server/routes/api');
-
-const port = 3000;
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.static('./dist/neo-explorer'));
 
-app.use('/api', api);
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
+app.get('/*', function(req, res) {
+  res.sendFile('index.html', {root: './dist/neo-explorer/'}
+);
 });
 
-app.listen(port, function() {
-    console.log('server running on localhost:' + port);
-});
+app.listen(process.env.PORT || 8080),
+	() => console.log("Server is running...");
